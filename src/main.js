33434c9624471ca82b65f296a0a1fd18bf488988
miguelnkono <nkonondememiguel7@gmail.com>
2025-05-25 00:44:25 +0100

    implemented the ability to add a new mark

--- src/main.js
+++ src/main.js
@@ -1,16 +1,41 @@
+import { add, get, getLast, Subject } from "./subject";
+
 const subject_name = document.getElementById("subject-name");
-const subject_mark = document.getElementById("subject-mark");
-const subject_type = document.getElementById("subject-option");
+const subject_mark_cc = document.getElementById("subject-mark-cc");
+const subject_mark_sn = document.getElementById("subject-mark-sn");
 const submit_btn = document.getElementById("submit");
+
+let count = 1;
 
 submit_btn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  console.log(subject_name.value);
-  console.log(subject_type.value);
-  console.log(subject_mark.value);
+  const name = subject_name.value;
+  const mark_cc = subject_mark_cc.value;
+  const mark_sn = subject_mark_sn.value;
 
-  subject_name.value = "";
-  subject_mark.value = "";
-  subject_type.value = "";
+  // calcul the total mark.
+  if (isNaN(mark_cc) || isNaN(mark_sn)) {
+    alert("the mark for the cc or the sn is not a number!!\nplease change it to number.");
+    return;
+  }
+
+  // convert to numeric.
+  const cc = Number(mark_cc);
+  const sn = Number(mark_sn);
+
+  const subject = new Subject(name, cc, sn);
+
+  if (add(subject)) {
+    document.getElementsByTagName("tbody")[0].innerHTML += `
+      <tr>
+          <td>${subject.idx}</td>
+          <td>${subject.name}</td>
+          <td>${subject.mark_cc}</td>
+          <td>${subject.mark_sn}</td>
+          <td>${subject.total}</td>
+      </tr>
+    `;   
+  }
+
 });
---
+++ src/subject.js
@@ -0,0 +1,123 @@
+// array of subjects
+let subjects = [];
+
+// index variable
+let idx = 0;
+
+function add (subject)
+{
+    if (subject instanceof Subject) 
+    {
+        if (subject.name === "") {
+            alert("You need to provide the name of the subject you want to add!");
+            return false;
+        }
+        if (subject.mark_cc > 20 || subject.mark_cc < 0) {
+            alert("The mark for the cc can not be less or greater than zero!");
+            return false;
+        }
+        if (subject.mark_sn > 20 || subject.mark_sn < 0) {
+            alert("The mark for the cc can not be less or greater than zero!");
+            return false;
+        }
+        subjects.push(subject);
+    }
+    else 
+    {
+        throw new Error(`the ${subject} is not an instance of the Subject class`);
+    }
+    return true;
+}
+
+function remove (index)
+{
+    const idx = subjects.findIndex(subject => subject.idx === index);
+    if (idx !== -1)
+    {
+        subjects = subjects.filter(subject => subject.idx !== index);
+    }
+}
+
+function update (idx, name, cc, sn)
+{
+    subjects.forEach(subject => {
+        if (subject.idx === idx) 
+        {
+            subject.name(name);
+            subject.mark_cc(cc);
+            subject.mark_sn(sn);
+        }
+    });
+}
+
+function get () 
+{
+    return subjects.map(sub => {
+        return `
+            <tr>
+              <td>${sub.idx}</td>
+              <td>${sub.name}</td>
+              <td>${sub.mark_cc}</td>
+              <td>${sub.mark_sn}</td>
+              <td>${sub.total}</td>
+            </tr>
+        `;
+    });
+    
+}
+
+function getLast ()
+{
+    return `
+        <tr>
+            <td>${subjects[idx].idx}</td>
+            <td>${subjects[idx].name}</td>
+            <td>${subjects[idx].mark_cc}</td>
+            <td>${subjects[idx].mark_sn}</td>
+            <td>${subjects[idx].total}</td>
+        </tr>
+    `;
+}
+
+// the subject class
+class Subject {
+  static count = 0;
+
+  #name;
+  #mark_cc;
+  #mark_sn;
+  #idx;
+  #total;
+
+  constructor(name, mark_cc, mark_sn) {
+    this.#name = name;
+    this.#mark_cc = mark_cc;
+    this.#mark_sn = mark_sn;
+    this.#idx = idx++;
+    this.computeTotal();
+  }
+
+  computeTotal ()
+  {
+    this.#total = (this.#mark_cc * 0.4) + (this.#mark_sn * 0.6);
+    return this.#total;
+  }
+
+  get name () { return this.#name; }
+  get mark_cc () { return this.#mark_cc; }
+  get mark_sn () { return this.#mark_sn; }
+  get idx () { return this.#idx; }
+  get total () { return this.#total; }
+
+  set name (new_name) { this.#name = new_name; }
+  set mark_cc (new_cc_mark) { 
+    this.#mark_cc = new_cc_mark;
+    this.computeTotal();
+  }
+  set mark_sn (new_sn_mark) { 
+    this.#mark_sn = new_sn_mark;
+    this.computeTotal();
+  }
+}
+
+export { Subject, get, update, add, remove, getLast, };
